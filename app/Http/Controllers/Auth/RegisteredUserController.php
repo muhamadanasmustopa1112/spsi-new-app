d42d1da4506be21fccbc7af84ef9b93144373444
muhamadanasmustopa <muhamadanasmustopa1112@gmail.com>
2024-08-28 15:05:03 +0700

    setup spatie

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,6 +43,9 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $user->assignRole('user');
+
+
         event(new Registered($user));
 
         Auth::login($user);
